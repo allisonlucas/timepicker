ca6cf57fa655ef5d9e9cb645c0d528e22320a294
Allison Lucas <adean@gci.com>
2018-07-16 13:14:03 -0600

    added some material-ui styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import DatePicker from 'react-datepicker';
+import { TextField, InputAdornment, Select } from '@material-ui/core';
+import { DateRange, ArrowDropDown } from '@material-ui/icons';
 import moment from 'moment';
+import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import logo from './logo.svg';
 import './App.css';
@@ -9,17 +11,27 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      startDate: null,
-      endDate: moment()
+      startDate: moment().date(1),
+      endDate: moment(),
+      startTime: moment().hour(0).minute(0),
+      endTime: moment()
     };
 
     this.handleChangeStart = this.handleChangeStart.bind(this);
     this.handleChangeEnd = this.handleChangeEnd.bind(this);
+    this.handleTimeChangeStart = this.handleTimeChangeStart.bind(this);
+    this.handleTimeChangeEnd = this.handleTimeChangeEnd.bind(this);
   }
 
   handleChangeStart(date) {
     this.setState({
       startDate: date
+    });
+  }
+
+  handleTimeChangeStart(time) {
+    this.setState({
+      startTime: time
     });
   }
 
@@ -29,43 +41,99 @@ class App extends Component {
     });
   }
 
+  handleTimeChangeEnd(time) {
+    this.setState({
+      endTime: time
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Awesome Date/Time Picker!</h1>
         </header>
-        <DatePicker
-          selected={this.state.startDate}
-          selectsStart
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onChange={this.handleChangeStart}
-          maxDate={moment()}
-          showTimeSelect
-          timeFormat="HH:mm"
-          dateFormat="LLL"
-          placeholderText="Select a Date & Time"
-          className='datePicker'
-          minTime={moment().hours(0).minutes(0)}
-          maxTime={moment()}
-        />
-
-        <DatePicker
-          selected={this.state.endDate}
-          selectsEnd
-          startDate={this.state.startDate}
-          endDate={this.state.endDate}
-          onChange={this.handleChangeEnd}
-          maxDate={moment()}
-          showTimeSelect
-          timeFormat="HH:mm"
-          dateFormat="LLL"
-          className='datePicker'
-          minTime={moment().hours(0).minutes(0)}
-          maxTime={moment()}
-        />
+        <div className='pickerWrapper'>
+          <DatePicker
+            customInput={
+              <TextField
+                InputProps={{
+                  startAdornment: <InputAdornment position="start"><DateRange/></InputAdornment>
+                }}
+              />
+            }
+            selected={ this.state.startDate }
+            selectsStart
+            startDate={ this.state.startDate }
+            endDate={ this.state.endDate }
+            onChange={ this.handleChangeStart }
+            maxDate={ moment() }
+            dateFormat="LL"
+            className='datePicker'
+          />
+          <DatePicker
+            customInput={
+              <TextField
+                InputProps={{
+                  endAdornment: <InputAdornment position="end"><ArrowDropDown/></InputAdornment>
+                }}
+              />
+            }
+            selected={ this.state.startTime }
+            onChange={ this.handleTimeChangeStart }
+            showTimeSelect
+            showTimeSelectOnly
+            timeFormat="HH:mm"
+            timeIntervals={ 15 }
+            dateFormat="LT"
+            timeCaption="Time"
+            openToDate={ moment() }
+            minTime={ moment().hours(0).minutes(0) }
+            maxTime={ moment() }
+            className='datePicker timePicker'
+          />
+        </div>
+        <div className='pickerWrapper'>
+          <DatePicker
+            customInput={
+              <TextField
+                InputProps={{
+                  startAdornment: <InputAdornment position="start"><DateRange/></InputAdornment>
+                }}
+              />
+            }
+            selected={ this.state.endDate }
+            selectsEnd
+            startDate={ this.state.startDate }
+            endDate={ this.state.endDate }
+            onChange={ this.handleChangeEnd }
+            maxDate={ moment() }
+            dateFormat="LL"
+            className='datePicker'
+          />
+          <DatePicker
+            customInput={
+              <TextField
+                InputProps={{
+                  endAdornment: <InputAdornment position="end"><ArrowDropDown/></InputAdornment>
+                }}
+              />
+            }
+            selected={ this.state.endTime }
+            onChange={ this.handleTimeChangeEnd }
+            showTimeSelect
+            showTimeSelectOnly
+            timeFormat="HH:mm"
+            timeIntervals={ 15 }
+            dateFormat="LT"
+            timeCaption="Time"
+            openToDate={ moment() }
+            minTime={ this.state.startTime || moment().hours(0).minutes(0) }
+            maxTime={ moment() }
+            className='datePicker timePicker'
+          />
+        </div>
       </div>
     );
   }
